40c90f7ebf557d3295c4a7de6ad03a894e3cbcc4
xdaco <xdaco.xinu@gmail.com>
2015-09-02 23:13:47 +0530

    Create events.h

---
+++ events.h
@@ -0,0 +1,19 @@
+ifndef EVENTS_H_ // include guard
+#define EVENTS_H_
+//Definition starts here
+//#include <...> // other dependencies
+//#include "..." // other dependencies
+
+#ifdef __cplusplus //making it transparent for C and C++ compiler
+extern “C” {
+#endif
+
+// ... My types, methods, variables starts here
+
+// .... My types, methods, variables ends here
+
+#ifdef __cplusplus // Ending extern C
+}
+#endif
+
+#endif // EVENTS_H_
